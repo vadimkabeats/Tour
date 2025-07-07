d15b181b375a41cc806493a3e6bf66aa64a79dba
Vadim-Li-V <vadker4@gmail.com>
2025-07-08 02:24:32 +0700

    Обновил WeatherRepository чтобы можно было узнавать погоду не по геолокации

--- app/src/main/java/com/example/tourguideplus/data/weather/WeatherRepository.kt
+++ app/src/main/java/com/example/tourguideplus/data/weather/WeatherRepository.kt
@@ -2,6 +2,12 @@ package com.example.tourguideplus.data.weather
 
 class WeatherRepository {
     private val api = WeatherService.create()
+
+    suspend fun getCurrentByCity(city: String, key: String): WeatherResponse =
+        api.getCurrentWeatherByCity(city, apiKey = key)
+
+    // Если понадобится геолокация
     suspend fun getCurrent(lat: Double, lon: Double, key: String): WeatherResponse =
         api.getCurrentWeather(lat, lon, apiKey = key)
 }
+
