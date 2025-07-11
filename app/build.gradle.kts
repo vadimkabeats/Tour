ef01a1d71adf2cc121e630029364860dc0a3c772
Vadim-Li-V <vadker4@gmail.com>
2025-07-11 20:52:51 +0700

    Добавил зависимость

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
+    implementation ("com.google.code.gson:gson:2.8.9")
 // RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
